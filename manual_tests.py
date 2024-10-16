adb85df11c3fb3256a235167ebca061d6583ca16
Jean-Olivier Irisson <irisson@normalesup.org>
2024-10-16 00:25:41 +0000

    Allow to use various input images in the manual tests

--- manual_tests.py
+++ manual_tests.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from importlib import reload
 
+test_img = "m_0595.jpg"
+test_img = "0be42a52fc61857c6a83eec1fffe485f_38088162.png"
+
 from zooprocess_multiple_separator import api
 reload(api)
 model, processor, device = api.warm()
@@ -11,12 +14,12 @@ model, processor, device = api.warm()
 from zooprocess_multiple_separator import utils
 reload(utils)
 masks, score, image = \
-    utils.predict_panoptic_masks(image_path="0be42a52fc61857c6a83eec1fffe485f_38088162.png",
+    utils.predict_panoptic_masks(image_path=test_img,
     model=model, processor=processor, device=device, min_mask_score=0.9, bottom_crop=0)
 print(score)
+plt.clf(); plt.imshow(image, cmap='Greys_r', interpolation='none'); plt.show()
 plt.clf(); plt.imshow(masks); plt.show()
-plt.clf(); plt.imshow(image); plt.show()
-sep_lines = utils.separate_with_watershed(panoptic_masks)
+sep_lines = utils.separate_with_watershed(masks)
 plt.clf(); plt.imshow(sep_lines); plt.show()
 
 # encode sep lines
@@ -26,7 +29,7 @@ shape = sep_lines.shape
 
 # reconstruct image
 X = np.zeros(shape)
-X[tuple(sep_coords)] = 1
+X[sep_coords] = 1
 plt.clf(); plt.imshow(X); plt.show()
 
 # Test API
@@ -34,7 +37,7 @@ class Img():
   def __init__(self, filename=None):
     self.filename=None
 image=Img()
-image.filename="0be42a52fc61857c6a83eec1fffe485f_38088162.png"
+image.filename=test_img
 
 res = api.predict(image=image, min_mask_score=0.9, bottom_crop=0)
 X = np.zeros(res['image_shape'])
