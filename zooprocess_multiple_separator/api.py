ab634f348f48242db3926f0c4989a0ac39aff44b
Jean-Olivier Irisson <irisson@normalesup.org>
2024-10-19 21:08:05 +0000

    Move output description inside api.py

--- zooprocess_multiple_separator/api.py
+++ zooprocess_multiple_separator/api.py
@@ -134,6 +134,28 @@ def get_predict_args():
     return arg_dict
 
 
+schema = {
+    "separation_coordinates": fields.List(
+        fields.List(fields.Int()),
+        required=True,
+        metadata={'description': """a list containing two other lists of ints: the x and y coordinates of pixels that draw lines on the original image, to separate multiple organisms. This list can be used to subset 2D arrays. For example, to create a black image with white separation lines, one can write:
+      import numpy as np
+      X = np.zeros(image_shape)
+      X[separation_coordinates] = 1
+      """}
+    ),
+    "image_shape": fields.Tuple(
+        (fields.Int(),fields.Int()),
+        required=True,
+        metadata={'description': "the height and width of the original image."}
+    ),
+    "score": fields.Float(
+        required=True,
+        metadata={'description': "an estimate of the confidence of the network for the quality of separation; this is very appropximate (and in [0,1])."}
+    )
+}
+
+
 @_catch_error
 def predict(**kwargs):
     """
@@ -143,7 +165,7 @@ def predict(**kwargs):
         See get_predict_args() above.
     
     Returns:
-        See api/__init__.py
+        See schema, above.
     """
 
     # get predicted masks
