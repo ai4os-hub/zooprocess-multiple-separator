c86674cccec58ed48f8faff998c6ef598f9ca012
Jean-Olivier Irisson <irisson@normalesup.org>
2025-10-31 11:48:32 +0100

    Remove the documentation of the output
    It did not work anyhow

--- zooprocess_multiple_separator/api.py
+++ zooprocess_multiple_separator/api.py
@@ -13,7 +13,6 @@ an exemplar module [2].
 from pathlib import Path
 import logging
 from webargs import fields
-from marshmallow import Schema
 
 import os
 import torch
@@ -146,33 +145,6 @@ def get_predict_args():
 
     return arg_dict
 
-
-# class OutputSchema(Schema):
-#     name = fields.Str()
-#     separation_coordinates = fields.Tuple(
-#         (fields.List(fields.Int()),fields.List(fields.Int())),
-#         required=True,
-#         metadata={'description': """a list containing two other lists of ints:\
-#         the x and y coordinates of pixels that draw lines on the original image,\
-#         to separate multiple organisms. This list can be used to subset 2D arrays.\
-#         For example, to create a black image with white separation lines, one can write:
-#       import numpy as np
-#       X = np.zeros(image_shape)
-#       X[separation_coordinates] = 1
-#       """}
-#     )
-#     image_shape = fields.Tuple(
-#         (fields.Int(),fields.Int()),
-#         required=True,
-#         metadata={'description': "the height and width of the original image."}
-#     )
-#     score = fields.Float(
-#         required=True,
-#         metadata={'description': "an estimate of the confidence of the network\
-#         for the quality of separation; this is very appropximate (and in [0,1])."}
-#     )
-# 
-# schema = fields.List(fields.Nested(OutputSchema))
 
 @_catch_error
 def predict(**kwargs):
